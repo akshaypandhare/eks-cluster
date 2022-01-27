4149c703574bee38bc8a681879afeecead28ee52
akshaypandhare <59005128+akshaypandhare@users.noreply.github.com>
2022-01-27 16:36:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ resource "aws_iam_openid_connect_provider" "default" {
   url   = aws_eks_cluster.this.identity.0.oidc.0.issuer
 
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [join("", data.tls_certificate.cluster.certificates.0.sha1_fingerprint)]
+  thumbprint_list = [data.tls_certificate.cluster.certificates.0.sha1_fingerprint]
 }
     
 ## EKS cluster IAM role.
